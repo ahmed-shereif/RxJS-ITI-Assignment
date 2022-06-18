b415d8d38b35e5439346621eefa8e2b9a2d1204b
ahmed sherif <ahmed1996sherif@gmail.com>
2022-06-18 04:28:22 +0200

    add observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Product } from './product';
 import { ProductCategory } from './product-category';
 import { Supplier } from './Supplier';
+import  pData from './MOCK_DATA.json'
+import { from, Observable, observable} from 'rxjs';
+import { delay, delayWhen, map, take, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +18,22 @@ export class AppComponent {
   productsCount: number = 0;
   categories: ProductCategory[];
   suppliers: Supplier[];
+  constructor(){
+
+  }
+  ngOnInit(){
+
+    const data=new Observable(obs=>{
+      let i=0
+      setInterval(()=>{
+
+        obs.next(pData[i])
+        i++;
+      },1000)
+
+    })
+    data.pipe(take(10),tap(d=>console.log(d))).subscribe({next: (item:Product)=>this.products.push(item)})
+  }
 
   onSelectedCategory(categoryId: string) {
 
